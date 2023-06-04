26fb867e49ab21d714ab1df33932a9c2aa85239c
dci-student <simaafshar1@gmail.com>
2023-06-04 17:52:43 +0200

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Pages } from './components/pages/Pages.jsx';
+
+
+function App() {
+  return (
+    <>
+     <Pages /> 
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/pages/Pages.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Header } from "../header/Header";
+import { BrowserRouter as Router, Routes} from "react-router-dom"
+ 
+
+
+
+export const Pages = () => {
+  return (
+    <>
+          <Router>
+        <Header />
+        <Routes>
+        
+        </Routes>
+       </Router>
+    </>
+  );
+};
